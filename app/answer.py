059edaaceb9d122a543a8bc043c7d4bc6022d2c2
mshankar58 <mayashankar227@gmail.com>
2020-11-04 15:37:19 -0500

    Small PEP8 fixes and code cleanup

--- app/answer.py
+++ app/answer.py
@@ -1,8 +1,9 @@
 from app.block import Block
 
+
 class Answer:
-  def __init__(self, mathid: str, version: int, problem: int, block: dict):
-    self.mathid = mathid
-    self.version = version
-    self.problem = problem
-    self.block = Block.from_json(block)
+    def __init__(self, mathid: str, version: int, problem: int, block: dict):
+        self.mathid = mathid
+        self.version = version
+        self.problem = problem
+        self.block = Block.from_json(block)
