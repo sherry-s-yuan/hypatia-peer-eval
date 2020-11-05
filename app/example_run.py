1e4b49a7dac7810bd5c163f209116ec7ed12ad50
ArKane <joshua.c.ong@hotmail.com>
2020-11-05 10:01:22 -0500

    Just to pull new changes

--- app/example_run.py
+++ app/example_run.py
@@ -3,5 +3,6 @@ from app.reader import Reader
 reader = Reader()
 assignment = reader.from_json_file('../example_files/block_example.json')
 
-# assignemnt -> answer -> block -> lines -> equation -> expression -> command and children expression
+# assignemnt -> answer -> block -> lines -> equation -> expression -> command
+# and children expression
 print(assignment.answers[0].block.lines[0].expressions[0].command)
--- app/reader.py
+++ app/reader.py
@@ -5,22 +5,23 @@ from app.answer import Answer
 
 
 class Reader:
-  def from_json_file(self, fn):
-    with open(fn) as f:
-      data = json.load(f)
-    return self.from_json_stream(data)
+    def from_json_file(self, fn):
+        with open(fn) as f:
+            data = json.load(f)
+        return self.from_json_stream(data)
 
-  def from_json_stream(self, data):
-    print(type(data))
-    docid = data['docid']
-    docname = data['docname']
-    userid = data['userid']
-    username = data['username']
-    assignemnt = Assignment(docid, docname, userid, username)
-    mathid = data['mathid']
-    version = data['version']
-    problem = data['problem']
-    expression = data['value']
-    answer = Answer(mathid, version, problem, expression)
-    assignemnt.add_answer(answer)
-    return assignemnt
+    @staticmethod
+    def from_json_stream(self, data):
+        print(type(data))
+        docid = data['docid']
+        docname = data['docname']
+        userid = data['userid']
+        username = data['username']
+        assignment = Assignment(docid, docname, userid, username)
+        mathid = data['mathid']
+        version = data['version']
+        problem = data['problem']
+        expression = data['value']
+        answer = Answer(mathid, version, problem, expression)
+        assignment.add_answer(answer)
+        return assignment
